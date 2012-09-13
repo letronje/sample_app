686c92dc517fa918514756968f573f76a32c5368
gaurav <gaurav.walia24@gmail.com>
2012-09-13 15:19:34 +0530

    Finish user edit,update,index and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,15 @@
 FactoryGirl.define do
-factory :user do
-name    "Gaurav Walia"
-email   "gaurav.walia24@gmail.com"
-password    "gaurav"
-password_confirmation   "gaurav"
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+    
+    factory :admin do
+        admin true
+    end
+  end
 end
-end
-
 
 
 
